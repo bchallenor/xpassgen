ad9a2bc2c458f955ee1c2aaba30710ffaa97781e
Ben Challenor <ben@challenor.org>
2013-07-06 09:35:22 +0100

    Initial commit

---
+++ src/main/scala/xpassgen/Program.scala
@@ -0,0 +1,25 @@
+package xpassgen
+
+import scala.io.Source
+import java.security.SecureRandom
+
+object Program extends App {
+  val baseWords = Source.fromFile("3esl.txt", "utf-8").getLines.toVector
+
+  def eligible(word: String): Boolean = (word.size >= 5) && (word.size <= 8) && (word forall (_.isLower))
+  val words = baseWords filter eligible
+  val wordCount = words.size
+  val wordBits = Math.log(wordCount) / Math.log(2)
+  println(f"Word list has $wordCount words, or about $wordBits%.1f bits per word")
+
+  val rng = new SecureRandom
+  println(s"Using SecureRandom: ${rng.getProvider} ${rng.getAlgorithm}")
+
+  println()
+
+  def nextWord = words(rng.nextInt(words.size))
+  val randomWords = Iterator continually nextWord
+  1 to 20 foreach { _ =>
+    println(randomWords take 4 mkString " ")
+  }
+}
